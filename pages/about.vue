614139d0a60a91f08ac6f09060a4cff3c027be07
muyi <38781672@qq.com>
2022-11-06 21:19:14 +0800

    chore：项目初始化

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+  
+</script>
+
+<template>
+  <div class="about">
+    关于
+  </div>
+</template>
+
+<style scoped lang="scss">
+  
+</style>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+  
+</script>
+
+<template>
+  <div class="">
+    ID
+  </div>
+</template>
+
+<style scoped lang="scss">
+  
+</style>
